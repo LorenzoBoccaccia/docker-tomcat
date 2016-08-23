36427cbdbad8bae1c00002b3de74137d4c146353
Lorenzo Boccaccia <lorenzo.boccaccia@gmail.com>
2016-08-23 14:45:00 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,27 @@
+#!/bin/sh
+
 #set up and log password
 
+if  grep -Fxq   "name=\"admin\" password=\"admin\"" /opt/apache-tomcat-8.5.4/conf/tomcat-users.xml
+then
 
+  PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  sed -i.bak "s/password=\"admin\"/password=\"$PASS\"/"  /opt/apache-tomcat-8.5.4/conf/tomcat-users.xml
+ 
+  
+fi
 
+PASS=$(cat /opt/apache-tomcat-8.5.4/conf/tomcat-users.xml | awk 'NR >= 9 && NR <= 9' | awk '{print substr( $0, 22, 60 )}')
+
+echo "*********************************************"
+echo "*********************************************"
+echo "*********************************************"
+echo "*********************************************"
+echo Manager $PASS
+echo "*********************************************"
+echo "*********************************************"
+echo "*********************************************"
+echo "*********************************************"
 
 
 
